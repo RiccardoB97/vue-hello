2a06e22b7906539c82e936d0ed0d18616ce15ff5
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-18 09:51:20 +0200

    Add image using :src

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let root = new Vue(
         el: '#root',
 
         data: {
-            message: "Hello this message is taken from the data property"
+            message: "Hello this message is taken from the data property",
+            image: "./img/meme.jpeg"
         }
     }
 )
